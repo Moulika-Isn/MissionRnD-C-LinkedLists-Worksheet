1fec94fa08515be4a959167d1a88dbb9b822a169
Moulika-Isn <Moulika-Isn@missionrnd.com>
2016-03-16 22:45:59 +0530

    Submit Commit

--- src/012sllSort.cpp
+++ src/012sllSort.cpp
@@ -21,6 +21,31 @@ struct node {
 	struct node *next;
 };
 
+void swap(struct node*a, struct node *b)
+{
+	int temp;
+	temp = a->data;
+	a->data = b->data;
+	b->data = temp;
+}
 void sll_012_sort(struct node *head){
-	
+	int swapped;
+	struct node *ptr1;
+	struct node *lptr = NULL;
+	do
+	{
+		swapped = 0;
+		ptr1 = head;
+
+		while (ptr1->next != lptr)
+		{
+			if (ptr1->data > ptr1->next->data)
+			{
+				swap(ptr1, ptr1->next);
+				swapped = 1;
+			}
+			ptr1 = ptr1->next;
+		}
+		lptr = ptr1;
+	} while (swapped);
 }
